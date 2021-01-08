661b2926842023abe9c634c07d01d100e23a802f
pront0 <muhammad.farhan@stmkg.ac.id>
2021-01-08 09:57:25 +0700

    dbKlim

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
     include 'connect.php';
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DataBase Klimatologi</title>
+        <link rel="stylesheet" href="Asset/style.css">
+    </head>
+    <body>
+        <div class="sidenav">
+            <div class="logo-side">
+                <img src="Asset/pict/bmkg.png" alt="logo-bmkg" class="logo-bmkg">
+                <h2>Database Klimatologi</h2>
+            </div>
+            <br>
+            <a href="index.html">Home</a>
+            <button></button>
+        </div>
+    </body>
+</html>
